48a075ce53efc0cfaec312e6c39174639b064d20
Sébastien Aubert <sebastien.aubert@heig-vd.ch>
2019-01-20 20:29:58 +0100

    Retrieve correct sign demand on MAP

--- src/pages/rdv-map/rdv-map.ts
+++ src/pages/rdv-map/rdv-map.ts
@@ -8,6 +8,7 @@ import {latLng, MapOptions, tileLayer, marker, Marker, Map, icon, popup, DomUtil
 import {CreateRDV} from "../../models/create-rdv";
 import {Storage} from "@ionic/storage";
 import {createScope} from "@angular/core/src/profile/wtf_impl";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'page-rdv-map',
@@ -19,14 +20,11 @@ export class RdvMapPage {
   map: Map;
   username = "";
   myRdvs;
-  currentPopup;
-  myRdvsLoaded;
-  othersRdvsLoaded
   markers;
-  otherRdvs: CreateRDV[];
+  otherRdvs;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient, public global: GlobalProvider,
-              private geolocation: Geolocation, private storage: Storage) {
+              private geolocation: Geolocation, private storage: Storage,private sanitizer:DomSanitizer) {
     const tileLayerUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     const tileLayerOptions = {maxZoom: 18};
     this.mapOptions = {
@@ -36,14 +34,8 @@ export class RdvMapPage {
       zoom: 13,
     };
     this.myRdvs = new Array();
-    this.currentPopup = {
-      title: "",
-      date: new Date(),
-      Description: ""
-    }
+    this.otherRdvs = new Array();
     this.markers = {};
-    this.myRdvsLoaded = false;
-    this.othersRdvsLoaded = false;
   }
 
   onMapReady(map: Map) {
@@ -67,34 +59,50 @@ export class RdvMapPage {
     this.storage.get('username').then((usernameGet) => {
       this.username = usernameGet;
       this.http.get(`${config.apiUrl}/users/${this.username}/rdv?username=true`, this.global.httpHeader).subscribe(rdvs => {
-        this.putRdvOnMap(rdvs, myRdvIcon);
-        this.myRdvsLoaded = true;
-        for(let rdv of this.myRdvs)
-        {
+        this.putRdvOnMap(rdvs, this.myRdvs);
+        for (let rdv of this.myRdvs) {
           var form = document.createElement('div');
-          console.log("id",rdv.id);
+          //TODO: list all guests
+          //TODO @AKaufi : styling
           form.innerHTML = `
                 <form id="${rdv.id}" >
                     <span >${rdv.title}</span>
-                    <span>${rdv.id}</span>
-                    <button type="submit">Sign Up </button>
+                    <span>TODO: guest</span>
                 </form>
             `;
           document.getElementById("myRdvsForms").appendChild(form);
-          this.markers = this.createmarker(rdv);
+          this.markers = this.createmarker(rdv, myRdvIcon);
         }
-
       });
     }).catch(err => console.warn(err))
     //Draw the others RDVS
     this.http.get(`${config.apiUrl}/rdvs?notmine=true`, this.global.httpHeader).subscribe(rdvs => {
-      this.putRdvOnMap(rdvs, otherRdvIcon);
-      this.othersRdvsLoaded = true;
+      this.putRdvOnMap(rdvs, this.otherRdvs);
+      for (let rdv of this.otherRdvs) {
+        var form = document.createElement('div');
+        //TODO @AKaufi : styling
+        form.innerHTML = this.sanitizer.bypassSecurityTrustHtml(`
+                <form class="popupForm" id="${rdv.id}" >
+                    <span >${rdv.title}</span>
+                    <span>${rdv.id}</span>
+                    <button type="button">Sign Up </button>
+                </form>
+            `).toString();
+        document.getElementById("myRdvsForms").appendChild(form);
+        document.getElementById(rdv.id).addEventListener('click',(theOne)=>{
+          //TODO: The code detect the correct rdv and the correct click. The ID of the event is in theOne
+          //1.make the API call to sign up to the event 2. Verification : Already on it ?If yes, how to disable the signUp BTN
+          console.log("Gros click", theOne);
+        })
+        this.markers = this.createmarker(rdv, otherRdvIcon);
+      }
 
     }, err => {
       console.error(err);
     });
+
   }
+
 
   /**
    * Draw markers on map
@@ -103,22 +111,32 @@ export class RdvMapPage {
    * @param rdvs
    * @param icon
    */
-  //TODO: Send the array in param
-  putRdvOnMap(rdvs, icon) {
+  putRdvOnMap(rdvs, rdvTable) {
     for (let key of Object.keys(rdvs)) {
       let rdv = rdvs[key];
       if (typeof rdv.lat === 'undefined' || typeof rdv.long === 'undefined')
         continue;
-      this.myRdvs.push({id: rdv._id, title: rdv.purposeTitle, date: rdv.date, description: rdv.description, lat:rdv.lat, long: rdv.long});
+      rdvTable.push({
+        id: rdv._id,
+        title: rdv.purposeTitle,
+        date: rdv.date,
+        description: rdv.description,
+        lat: rdv.lat,
+        long: rdv.long
+      });
     }
   }
 
-  createmarker(LErdv) {
-    let lemarker = marker([LErdv.lat, LErdv.long], {icon: otherRdvIcon}).addTo(this.map).bindPopup(document.getElementById(LErdv.id));
+  createmarker(LErdv, icon) {
+    let lemarker = marker([LErdv.lat, LErdv.long], {icon: icon}).addTo(this.map).bindPopup(document.getElementById(LErdv.id));
     return lemarker;
   }
-}
 
+  onSubmit($event) {
+    $event.preventDefault();
+    console.log("try to submit", $event);
+  }
+}
 
 //TODO @AKaufi : styling
 var otherRdvIcon = icon({
